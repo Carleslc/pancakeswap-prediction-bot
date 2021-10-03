89bc9c115186f51bb8be78b83c39fc039ff2d409
Carleslc <lazaro.costa.carles@gmail.com>
2021-10-03 04:15:29 +0200

    data refactor

--- data.py
+++ data.py
@@ -1,13 +1,18 @@
 import os
-import csv
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+from pathlib import Path
 
 from dotenv import load_dotenv
 from binance.client import Client
 
-DATA_PATH = './data/'
+DATA_PATH = Path('data')
+LENGTH = '7 day'
+INTERVAL = Client.KLINE_INTERVAL_1MINUTE
+DATA_FILE = 'bnbusdt_' + INTERVAL
 
 API = None # Client
 
@@ -19,6 +24,8 @@ def load_api_client() -> Client:
   global API
 
   if not API:
+    print("Loading Binance API...")
+
     load_dotenv()
 
     api_key = os.getenv('API_KEY')
@@ -31,47 +38,58 @@ def load_api_client() -> Client:
 
     API = Client(api_key, api_secret)
 
-def get_data(symbol = 'BNBUSDT', length = '7 day', interval = Client.KLINE_INTERVAL_1MINUTE):
+def get_data(symbol = 'BNBUSDT', length = LENGTH, interval = INTERVAL) -> pd.DataFrame:
   print(f"Fetching data from Binance ({symbol} {length} {interval})...")
-  return API.get_historical_klines(symbol, interval, f'{length} UTC')
 
-def ohlcv_csv_write(f, data):
-  csv_writer = csv.writer(f)
-  csv_writer.writerow([
+  data = API.get_historical_klines(symbol, interval, f'{length} UTC') # list of lists
+  data = np.array(data) # convert to numpy 2D array (rows, columns)
+  data = data[:, :-1] # skip last 'ignore' column on numpy array
+
+  columns = [
     'open_time', 'open', 'high', 'low', 'close', 'volume',
     'close_time', 'quote_asset_volume', 'number_of_trades',
-    'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume',
-    'ignore'
-  ])
-  csv_writer.writerows(data)
+    'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume'
+  ]
 
-def save_data(path, data, writer = ohlcv_csv_write):
+  return pd.DataFrame(data, columns=columns)
+
+def get_data_file_path(filename: str) -> Path:
+  return DATA_PATH / f'{filename}.csv'
+
+def exists_data(filename: str) -> bool:
+  return get_data_file_path(filename).exists()
+
+def save_data(filename: str, data: pd.DataFrame):
+  path = get_data_file_path(filename)
   os.makedirs(DATA_PATH, exist_ok=True)
-
-  with open(path, 'w') as f:
-    writer(f, data)
-
+  data.to_csv(path, index=False, header=True)
   print(f"Saved data: {path}")
 
-def load_data(path, columns = None):
+def load_data(filename: str, columns: list[str] = None) -> pd.DataFrame:
+  print("Loading data...")
+  path = get_data_file_path(filename)
   data = pd.read_csv(path)
-  df = pd.DataFrame(data, columns=columns)
+  data = pd.DataFrame(data, columns=columns)
   print(f"Loaded: {path}")
-  return df
+  return data
 
-if __name__ == "__main__":
-  data_file = DATA_PATH + 'bnbusdt_training.csv'
-
-  if not os.path.exists(data_file):
-    load_api_client()
-    save_data(data_file, get_data())
-
-  df = load_data(data_file, ['close_time', 'close'])
-
+def plot_data(data: pd.DataFrame):
   TIMEZONE = 'Europe/Madrid'
 
-  df['close_time'] = pd.DatetimeIndex(pd.to_datetime(df['close_time'], unit='ms')).tz_localize('UTC').tz_convert(TIMEZONE)
+  data['close_time'] = pd.DatetimeIndex(pd.to_datetime(data['close_time'], unit='ms')).tz_localize('UTC').tz_convert(TIMEZONE)
 
-  plt.title("BNBUSDT Training Data")
-  plt.plot(df['close_time'], df['close'], color='green')
+  plt.figure(figsize=(10,5))
+  plt.title("BNB/USDT Training Data")
+  plt.xlabel('Date')
+  plt.ylabel('Price')
+  plt.plot(data['close_time'], data['close'], color='green')
   plt.show()
+
+if __name__ == "__main__":
+  if not exists_data(DATA_FILE):
+    load_api_client()
+    save_data(DATA_FILE, get_data())
+
+  data = load_data(DATA_FILE, ['close_time', 'close'])
+
+  plot_data(data)
